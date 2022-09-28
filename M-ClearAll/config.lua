78b8d2caaede1c61f9881ac3104350077d097edc
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2022-09-28 18:28:08 +0100

    Add files via upload

---
+++ M-ClearAll/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.ClearAll = {
+    Ace = "furiousfoxgg.clearall",
+    Command = "clearall",
+    Discord = {
+        Webhook = "",
+        ServerName = "FuriousFoxGG Development",
+    }
+}
